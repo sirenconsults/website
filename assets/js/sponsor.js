5bd3f3e7b307da8f3e82b88f8ea5f336ee42e432
ugfire <ugfirecompany@gmail.com>
2024-07-15 16:32:17 +0300

    first commit

---
+++ assets/js/sponsor.js
@@ -0,0 +1,20 @@
+Swal.fire({
+  title: " Start a building project?",
+  html: `
+  <h1 class="text-center display-1 "> <span class="bi bi-pen-fill text-warning"></span></h1>    
+ 
+  <p><a class="btn btn-light mr-2 m-2" onclick="Swal.close()">Not Yet</a><a class="btn btn-warning m-2" onclick="contact()">Yes please</a></p>
+     
+    `,
+  showCloseButton: true,
+  showCancelButton: false,
+  showConfirmButton: false,
+});
+
+function close() {
+  Swal.close();
+}
+
+function contact() {
+  window.location.href = "/contact";
+}
