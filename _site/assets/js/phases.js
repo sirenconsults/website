e7d1c7c27ae5d795280a6d04b504c5bf81b11e45
ugfire <ugfirecompany@gmail.com>
2024-07-31 06:43:04 +0300

    added sketches of tempean

---
+++ _site/assets/js/phases.js
@@ -0,0 +1,7 @@
+var options = document.getElementById("options");
+var tempean = document.getElementById("tempean");
+
+function showtempean() {
+  options.style.display = "none";
+  tempean.style.display = "block";
+}
