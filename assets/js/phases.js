5eb93c50c2e750ad819a5422d19cc620150aec6f
ugfire <ugfirecompany@gmail.com>
2024-10-06 02:42:48 +0300

    added gate phases and comments on blogs

--- assets/js/phases.js
+++ assets/js/phases.js
@@ -1,7 +1,15 @@
 var options = document.getElementById("options");
 var tempean = document.getElementById("tempean");
+var gate = document.getElementById("gate");
 
 function showtempean() {
   options.style.display = "none";
   tempean.style.display = "block";
+  gate.style.display = "none";
+}
+
+function showgate() {
+  options.style.display = "none";
+  gate.style.display = "block";
+  tempean.style.display = "none";
 }
